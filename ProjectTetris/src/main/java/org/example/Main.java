e8d8bc9b5cdad0f5c4f092937c7b09f74386a4dd
Tom <tom.mcmillan@griffithuni.edu.au>
2025-07-23 14:26:20 +1000

    ProjectSetup

---
+++ ProjectTetris/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello from JavaFX + Maven!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Maven Project");
+        primaryStage.show();
+    }
+}
